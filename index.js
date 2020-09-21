0c778e2f71dfb76126ee026f39d2569505369b1d
Suneet Jena <suneetjena@Suneets-MacBook-Air.local>
2020-09-21 22:49:23 +0530

    set prod env variables

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
-const app = express();
+const authRoutes = require('./routes/authRoutes');
+const {mongoURI, cookieKey} = require('./config/keys')
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
-app.get('/', (req, res) => {
-    
-    res.send({cool : "deploy"});
-})
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(mongoURI);
+
+const app = express();
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60*1000,
+    keys: [cookieKey]
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+ authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); 
---
+++ services/passport.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { googleClientId, googleClientSecret } = require('../config/keys')
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, end) => {
+    return end(null, user.id);
+})
+
+passport.deserializeUser((id, end) => {
+    User.findById(id)
+        .then(user => {
+             end(null, user);
+        })
+ })
+
+passport.use(new GoogleStrategy({
+    clientID: googleClientId,
+    clientSecret: googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, end) => {
+        
+        User.findOne({
+            googleId : profile.id
+        })
+        .then(existingUser => {
+            if (existingUser) {
+                end(null, existingUser);
+            } else {
+                new User({
+                    googleId: profile.id
+                })
+                .save()
+                .then(user => {
+                    end(null, user);
+                })
+             }
+        })
+         
+    })
+);
+
+ 
